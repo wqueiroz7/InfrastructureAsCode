9c6accd59f4f131759e92a77bc74bd9625171f0d
Wilson Queiroz <w7queiroz@gmail.com>
2022-01-22 16:50:41 -0300

    Creating infrastructure on aws with Terraform

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+      aws = {
+          source = "hashicorp/aws",
+          version = "~> 3.27"
+      }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0892d3c7ee96c0bf7"
+  instance_type = "t2.micro" 
+  key_name      = "iac-alura" 
+
+  tags = {
+    Name = "Primeira Instancia"
+  }
+}
